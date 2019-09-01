c729d1c95e35a784820a4b45d03f8d63f8a90b36
TK Vishal <vishalsanthosh1999@gmail.com>
2019-09-01 06:41:02 +0530

    changed readme

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ const { app } = require("electron");
 const Datastore = require("nedb-promises");
 const dbFactory = fileName =>
   Datastore.create({
-    filename: `${
-      process.env.NODE_ENV === "dev" ? "." : app.getAppPath("userData")
-    }/data/${fileName}`,
+    filename: `./data/${fileName}`,
     timestampData: true,
     autoload: true
   });
--- mwctrl.js
+++ mwctrl.js
@@ -1,12 +1,56 @@
+const db = require("./db");
+
+const UserReg = async data => {
+  const obj = await db.userinfo.insert({ data });
+  return obj;
+};
+
 let isfirst = localStorage.getItem("isfirst", true);
 if (!isfirst) {
   document.getElementById("dashboard-wrap").style.display = "none";
+} else {
+  document.getElementById("form-wrap").style.display = "none";
+  document.getElementById("dashboard-wrap").style.display = "block";
 }
 
 let submitbtn = document.getElementById("submit-btn");
 let form = document.getElementById("form-wrap");
 
 submitbtn.addEventListener("click", e => {
-  form.style.display = "none";
-  document.getElementById("dashboard-wrap").style.display = "block";
+  var name = document.getElementById("name-ipc").value;
+  var gender = document.getElementById("gender-ipc").value;
+  if (document.getElementById("standing-desk-ipc").checked) {
+    var standing = document.getElementById("standing-desk-ipc").value;
+  }
+  if (document.getElementById("sit-ipc").checked) {
+    var sit = document.getElementById("sit-ipc").value;
+  }
+  var roomspc = document.getElementById("room-ipc").value;
+  var freetime = document.getElementById("free-ipc").value;
+
+  var orientation;
+  if (sit == "on") {
+    orientation = "sit";
+  } else if (standing == "on") {
+    orientation = "stand";
+  }
+
+  const userjson = {
+    name: name,
+    gender: gender,
+    orientation: orientation,
+    roomspc: roomspc,
+    freetime: freetime
+  };
+
+  UserReg(userjson)
+    .then(val => {
+      console.log(val);
+      form.style.display = "none";
+      localStorage.setItem("isfirst", true);
+      document.getElementById("dashboard-wrap").style.display = "block";
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
